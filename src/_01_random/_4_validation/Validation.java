34d86cea0c60479a787cacaf120cd4c0f7d925fb
league <league@imac-29.attlocal.net>
2023-07-21 18:28:56 -0700

    Added fixes to Validation, needs completion next class.

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -18,11 +18,19 @@ public class Validation {
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 		if  (randomNumber==0) {
-		String JOptionPane.showMessageDialog(null,"You look good today!");
+			JOptionPane.showMessageDialog(null,"You look good today!");
 		}
-		
+		else if  (randomNumber==1) {
+			JOptionPane.showMessageDialog(null,"Ready for a good day? Yes you are! ");
+			
+			else if  (randomNumber==2) {
+				JOptionPane.showMessageDialog(null,"Ready for a good day? Yes you are! ");
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
 	}
-
+	}	
+}
+}
+}
