eb2d50e03de7350bc844137906ef8833f9c701c3
league <league@imac-12.attlocal.net>
2023-09-01 18:22:26 -0700

    Made more progress on LotteryNumbers. Osvaldo's back! :sunglasses:

--- src/_01_random/_6_lottery_numbers/LotteryNumbers.java
+++ src/_01_random/_6_lottery_numbers/LotteryNumbers.java
@@ -2,23 +2,32 @@ package _01_random._6_lottery_numbers;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class LotteryNumbers {
 	public static void main(String[] args) {
 
 		Random randomMaker = new Random();
-		int randomNumber0 = randomMaker.nextInt(100);
+		Random e = randomMaker;
+		Random lottery = e;
+		for(int i = 0; i < 6; i++) {
+			Random e1 = randomMaker;
+			Random e11 = lottery;
 
-		int randomNumber1 = randomMaker.nextInt(100);
-		
-		int randomNumber2 = randomMaker.nextInt(100);
-		
-		int randomNumber3 = randomMaker.nextInt(100);
-		
-		int randomNumber4 = randomMaker.nextInt(100);
-		
-		int randomNumber5 = randomMaker.nextInt(100); 
-
-		JOptionPane.showMessageDialog(null,"")
-
+		}
+		JOptionPane.showMessageDialog(null,lottery);
 	}
-}
+}	
+
+
+//int randomNumber0 = randomMaker.nextInt(100);
+
+// int randomNumber1 = randomMaker.nextInt(100);
+
+//int randomNumber2 = randomMaker.nextInt(100);
+
+//int randomNumber3 = randomMaker.nextInt(100);
+
+//int randomNumber4 = randomMaker.nextInt(100);
+
+//int randomNumber5 = randomMaker.nextInt(100); 
