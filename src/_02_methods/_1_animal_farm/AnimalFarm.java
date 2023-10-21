e1320bd39b1a8662b5a64a03d072b3894d484775
league <maddoxseul@sbsd.net>
2023-10-20 18:28:07 -0700

    Finised.

--- src/_02_methods/_1_animal_farm/AnimalFarm.java
+++ src/_02_methods/_1_animal_farm/AnimalFarm.java
@@ -10,7 +10,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class AnimalFarm {
-	
+
 	/***********  SOUND ***************
 	 * Some computers are unable to play sounds. 
 	 * If you cannot play sound on this computer, set canPlaySounds to false.
@@ -19,40 +19,72 @@ public class AnimalFarm {
 	boolean canPlaySounds = true;
 	private Object moo;
 
-	
+
 	public static void main(String[] args) {
 		new AnimalFarm().animals();
 	}
-	
+
 	void animals() {
-int input;
-int input1;
+		//int input;
+		//int input1;
 		/* 1. Ask the user which animal they want, then see and hear 
 		 *    the animal they chose using one of the methods below.
-		*/			 
-			
-String input11=	JOptionPane.showInputDialog(null,"Welcome to the Animal Farm! Which Animal do you want to see and hear?");
+		 */			 
+
+		String input11=	JOptionPane.showInputDialog(null,"Welcome to the Animal Farm! Which Animal do you want to see and hear?");
 		/* 2. Make it so that the user can keep entering new animals. */
 
 
-String input111= JOptionPane.showInputDialog(null,"Enter new animals");
+		//String input111= JOptionPane.showInputDialog(null,"Enter new animals");
+		//
+		// JOptionPane.showMessageDialog(null,");
+		if (input11.equals("cow")) {
+			moo();
+		}
 
- JOptionPane.showMessageDialog(null,"Here's it all!");
-playNoise(mooFile);
-playNoise(meowFile);
-playNoise(llamaFile);
-playNoise(quackFile);
-playNoise(woofFile);
-	}	
+		if (input11.equals("duck")) {
+			quack();
+
+		}	
+
+		if (input11.equals("dog")) {
+			woof();
+		}
+
+		if (input11.equals("cat")) {
+			meow();
+		}
+	
+		if (input11.equals("llama")) {
+			llamaScream();
+		}
 	
 
-	//private void showMessageDialog(Object object, String string) {
+
+
+
+
+} 
+private void llama() {
 		// TODO Auto-generated method stub
 		
-	
+	}
+
+//do not put anything to interfere with curly brace, this ends ANIMALS.
+	//  Nick can you stop spying on me ;) thanks 
+
+
+	//private void showMessageDialog(Object object, String string) {
+	// TODO Auto-generated method stub
+	//playNoise(mooFile);
+	//playNoise(meowFile);
+	//playNoise(llamaFile);
+	//playNoise(quackFile);
+	//playNoise(woofFile);
+
 
 	void moo() {
-		
+
 		playNoise(mooFile);
 		showImage(cowIcon);
 	}
@@ -66,20 +98,20 @@ playNoise(woofFile);
 		playNoise(woofFile);
 		showImage(dogIcon);
 	}
-	
+
 	void meow() {
 		playNoise(meowFile);
 		showImage(catIcon);
 	}
-	
+
 	void llamaScream() {
 		playNoise(llamaFile);
 		showImage(llamaIcon);
 	}
-	
 
 
-	
+
+
 
 	/* Ignore this Osvaldo Ruiz spy crap */
 	String path = "src/_02_methods/_1_animal_farm/";
@@ -93,26 +125,26 @@ playNoise(woofFile);
 	ImageIcon dogIcon;
 	ImageIcon duckIcon;
 	ImageIcon cowIcon;
-	
+
 	private void showImage (ImageIcon icon) {
-			JOptionPane.showMessageDialog(null, "", "You chose", 0, icon);
+		JOptionPane.showMessageDialog(null, "", "You chose", 0, icon);
 	}
-	
+
 	private void playNoise(String soundFile) {
 		if (canPlaySounds) {	
 			File sound = new File(path+soundFile);
 			if (sound.exists()) {
 				new Thread(() -> {
-				try {
-					Clip clip = AudioSystem.getClip();
-					clip.open(AudioSystem.getAudioInputStream(sound));
-					clip.start();
-					Thread.sleep(clip.getMicrosecondLength()/1000);
-				}
-				catch (Exception e) {
-					System.out.println("Could not play this sound");
-				}}).start();
-	 		}
+					try {
+						Clip clip = AudioSystem.getClip();
+						clip.open(AudioSystem.getAudioInputStream(sound));
+						clip.start();
+						Thread.sleep(clip.getMicrosecondLength()/1000);
+					}
+					catch (Exception e) {
+						System.out.println("Could not play this sound");
+					}}).start();
+			}
 			else {
 				System.out.println("File does not exist");
 			}
@@ -127,7 +159,7 @@ playNoise(woofFile);
 			duckIcon = new ImageIcon(path+"duck.jpg");
 
 		} catch (Exception e) {
-					
+
 		}
 	}
 }
