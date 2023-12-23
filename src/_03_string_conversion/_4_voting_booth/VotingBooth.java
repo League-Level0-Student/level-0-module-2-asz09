c6c302d8215938dba332d78c7fb303c250570142
league <league@adminisorsimac3.attlocal.net>
2023-12-22 18:23:07 -0800

    Fnished Roller Coaster :smile: :sunglasses
    Started voting booth

---
+++ src/_03_string_conversion/_4_voting_booth/VotingBooth.java
@@ -0,0 +1,15 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class VotingBooth {
+public static void main(String[] args) {
+	
+	
+	JOptionPane.showInputDialog(null,"How old are you");
+	int age = 18;
+	if age = 18 {
+		JOptionPane.showInputDialog(null,you can Vote);
+	}
+}
+}
