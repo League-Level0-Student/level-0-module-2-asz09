d12fd30eb5e2868b071e002d9c29bffc3135d658
league <sciencekid@quakefeed.com>
2024-01-05 18:20:29 -0800

    VotingBooth is complete, no Nick needed today. :smile:

--- src/_03_string_conversion/_4_voting_booth/VotingBooth.java
+++ src/_03_string_conversion/_4_voting_booth/VotingBooth.java
@@ -3,13 +3,17 @@ package _03_string_conversion._4_voting_booth;
 import javax.swing.JOptionPane;
 
 public class VotingBooth {
-public static void main(String[] args) {
-	
-	
-	JOptionPane.showInputDialog(null,"How old are you");
-	int age = 18;
-	if age = 18 {
-		JOptionPane.showInputDialog(null,you can Vote);
+	public static void main(String[] args) {
+
+		String age = JOptionPane.showInputDialog(null, "How old are you? \n You must be over 18 to vote");
+
+		int input = Integer.parseInt(age);
+		if (input >= 18) {
+			JOptionPane.showMessageDialog(null, "You are able to vote");
+
+		}
+		else {
+		JOptionPane.showMessageDialog(null,"You are not allowed to vote according to law.");
+		}
 	}
-}
 }
