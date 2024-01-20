522c6511c96b0c046038fabcb63b9f7d8601a816
league <league@imac-20.attlocal.net>
2024-01-19 18:24:01 -0800

    :smile:

---
+++ src/_04_spot_the_error/test.java
@@ -0,0 +1,12 @@
+package _04_spot_the_error;
+
+import java.util.Random;
+
+public class test {
+	static Random test=new Random();
+public static void main(String[] args) {
+	
+	
+	System.out.println(test.nextInt(5));
+}
+}
